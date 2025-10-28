080e79616fc3ca94d78c06ac20f148a0ee8ad4fb
Andrew Solomko <andrii.solomko2007@outlook.com>
2025-10-28 14:32:31 -0700

    Added margin, urgency, and priority functions to main.js (not tested)

--- src/main.js
+++ src/main.js
@@ -1,7 +1,42 @@
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import 'bootstrap';
 
-function sayHello() {
-    
+// Use urgency level and importance level to calculate priority
+function calculatePriority(urgency, importance) {
+  // urgencyMultiplier is needed because urgency take priority over same level importance
+  let urgencyMultiplier = 1.2;
+  let priorityLevel = urgency * urgencyMultiplier + importance;
+  return priorityLevel;
 }
+
+// Use time margin to calculate urgency level
+function calculateUrgency(margin) {
+  let urgency = null;
+  switch (true) {
+    case margin < 0.15:
+      urgency = 1;
+      break;
+    case margin < 0.3:
+      urgency = 2;
+      break;
+    case margin < 0.45:
+      urgency = 3;
+      break;
+    case margin < 0.6:
+      urgency = 4;
+      break;
+    default:
+      urgency = 5;
+      break;
+  }
+  return urgency;
+}
+
+// Use time needed, time left to calculate margin
+function calculateSlackMargin(timeNeeded, timeAvailable) {
+  let margin = timeNeeded / timeAvailable;
+  return margin;
+}
+
+function sayHello() {}
 // document.addEventListener('DOMContentLoaded', sayHello);
